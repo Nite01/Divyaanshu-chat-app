8fb0c34beca5f3ef6a74c021351cf174a850282c
Nitesh Pathak <113054500+Nite01@users.noreply.github.com>
2025-08-03 00:33:26 +0530

    hehe
    hehe

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_REACT_APP_BACKEND_BASE_URL || "http://localhost:5001";
+
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? `${import.meta.env.VITE_REACT_APP_BACKEND_BASE_URL}/api` : "/api",
+    baseURL: `${BASE_URL}/api`,
     withCredentials: true,
-})
+});
